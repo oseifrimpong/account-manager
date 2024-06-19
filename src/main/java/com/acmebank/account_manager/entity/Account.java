8fcb863d0ca48256b29360056f63bc86a1cee09e
oseifrimpong <obedosei@gmail.com>
2024-06-19 14:14:31 +0800

    feat: all implementation done

--- src/main/java/com/acmebank/account_manager/entity/Account.java
+++ src/main/java/com/acmebank/account_manager/entity/Account.java
@@ -1,6 +1,7 @@
 package com.acmebank.account_manager.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 @Getter
 @Setter
 @RequiredArgsConstructor
